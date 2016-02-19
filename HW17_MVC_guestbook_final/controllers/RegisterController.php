4f138d69c134a44fd9a47398b4d56597c1dd07d8
Vorobey <dev@vorobey.me>
2016-02-19 16:55:13 +0200

    HW17_from_15.02.2-16

---
+++ HW17_MVC_guestbook_final/controllers/RegisterController.php
@@ -0,0 +1,42 @@
+<?php
+
+class RegisterController extends BaseController {
+    public function execute($arguments = []) {
+
+        if (
+            isset($_POST['username']) &&
+            isset($_POST['password']) &&
+            isset($_POST['password-confirm']) &&
+            !empty($_POST['username']) &&
+            !empty($_POST['password'])
+
+        ) {
+            if ($_POST['password'] == $_POST['password-confirm']) {
+                $fdb = new FileDB(__DIR__ . '/../db');
+                if (!$fdb->findUsername($_POST['username'])) {
+                    $fdb->addUser($_POST['username'], $_POST['password']);
+                    UserSession::getInstance()->login($_POST['username']);
+                    Router::redirect('/');
+                } else {
+                    $error = "Failed: User already exists.";
+                }
+            } else {
+                $error = "Failed: Password does not match.";
+            }
+        } else if(
+            isset($_POST['username']) &&
+            isset($_POST['password']) &&
+            isset($_POST['password-confirm'])
+        ) {
+            $error = "Failed: All fields is required.";
+        }
+
+        require_once 'views/parts/header.php';
+
+        require_once 'views/register.php';
+
+        require_once 'views/parts/footer.php';
+
+        return true;
+    }
+}
---
+++ HW17_MVC_guestbook_final/index.php
@@ -0,0 +1,17 @@
+<?php
+
+require_once 'components/Router.php';
+require_once 'components/UserSession.php';
+require_once 'controllers/BaseController.php';
+require_once 'components/Pagination.php';
+
+require_once 'models/Picture.php';
+require_once 'models/FileDB.php';
+
+
+
+$router = new Router($_SERVER['REQUEST_URI']);
+
+if(!$router->handle()) {
+    echo 'Path not found.';
+}
